75d728204e4073f7abf4dbed2c618a2dd2e26136
ye-guo <aidjajd@163.com>
2024-07-28 23:42:31 +0800

    feat: 增加post文件上传处理

--- API_backend/src/main/java/icu/yeguo/yeguoapi/controller/InterfaceInfoController.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/controller/InterfaceInfoController.java
@@ -15,7 +15,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -87,47 +90,61 @@ public class InterfaceInfoController {
         return ResultUtils.success(interfaceInfoVOList);
     }
 
+
     @PostMapping("onlineInvoking")
-    public Result<String> onlineInvoking(@RequestBody InvokingRequest invokingRequest,
-                                         @RequestParam("accessKey") String accessKey,
-                                         @RequestParam("signature") String signature) {
+    public Result<String> onlineInvoking(@RequestParam("accessKey") String accessKey,
+                                         @RequestParam("signature") String signature,
+                                         @RequestPart("invokingRequest") InvokingRequest invokingRequest,
+                                         @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
         /*
          * InvokingRequest: {irp:[{},{}],method:"string",url:"string"}
          * */
         log.info("invokingRequest:" + invokingRequest);
         log.info("irp:" + Arrays.toString(invokingRequest.getIrp()));
-
         log.info("accessKey:" + accessKey);
         log.info("signature:" + signature);
 
         InvokingRequestParams[] irp = invokingRequest.getIrp();
         String result = null;
+        HashMap<String, Object> paramMap = new HashMap<>();
+
         // GET 请求
         if ("GET".equals(invokingRequest.getMethod())) {
-            HashMap<String, Object> paramMap = new HashMap<>();
+            // 请求中携带的请求参数,网关处验证
+            paramMap.put("accessKey", accessKey);
+            paramMap.put("signature", signature);
+            // 接口中定义的请求参数，接口处调用
             for (InvokingRequestParams item : irp) {
                 paramMap.put(item.getName(), item.getValue());
             }
-            // 请求中带的请求参数
-            paramMap.put("accessKey", accessKey);
-            paramMap.put("signature", signature);
             result = HttpRequest.get(invokingRequest.getUrl())
                     .form(paramMap)//表单内容
                     .timeout(6 * 60 * 60 * 1000)//超时，毫秒
                     .execute().body();
+        } else if ("POST".equals(invokingRequest.getMethod())) {
+            paramMap.put("accessKey", accessKey);
+            paramMap.put("signature", signature);
+            for (InvokingRequestParams item : irp) {
+                paramMap.put(item.getName(), item.getValue());
+            }
+            if (file != null) {
+                File tempFile = File.createTempFile("upload", file.getOriginalFilename());
+                file.transferTo(tempFile);
+                paramMap.put("file", tempFile);
+                System.out.println(paramMap);
+                result = HttpRequest.post(invokingRequest.getUrl())
+                        .header("Content-Type", "multipart/form-data")
+                        .form(paramMap)
+                        .timeout(6 * 60 * 60 * 1000)
+                        .execute().body();
+                System.out.println(result);
+            }else {
+                result = HttpRequest.post(invokingRequest.getUrl())
+                        .form(paramMap)
+                        .timeout(6 * 60 * 60 * 1000)
+                        .execute().body();
+            }
         }
-
-        // POST 请求
-//        if ("POST".equals(invokingRequest.getMethod())) {
-//            HashMap<String, Object> paramMap = new HashMap<>();
-//            for (InvokingRequestParams item : irp) {
-//                paramMap.put(item.getName(), item.getValue());
-//
-//            }
-//            result= HttpUtil.post(invokingRequest.getUrl(), paramMap);
-//        }
-
         return ResultUtils.success(result);
     }
-
 }
