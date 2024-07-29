9c68122487aba6fee15b07966cb1b09ad6fd8533
ye-guo <aidjajd@163.com>
2024-07-29 18:01:41 +0800

    refactor: 重构代码

--- API_interface/src/main/java/icu/yeguo/yeguoapiinterface/controller/InterfaceServiceController.java
+++ API_interface/src/main/java/icu/yeguo/yeguoapiinterface/controller/InterfaceServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,45 +18,35 @@ public class InterfaceServiceController {
     @GetMapping("qq/info")
     public String getQQInfo(@RequestParam("qq") Long qq) {
         log.info("请求到==>/api/qq/info接口==qq:" + qq);
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("qq", qq);
-        return HttpUtil.get("https://api.oioweb.cn/api/qq/info", paramMap);
+        return executeGetRequest("https://api.oioweb.cn/api/qq/info","qq",qq);
     }
 
     // 获取ip地址
     @GetMapping("ip/ipaddress")
     public String getIpAddress(@RequestParam("ip") String ip) {
         log.info("请求到==>/api/ip/ipaddress接口==ip" + ip);
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("ip", ip);
-        return HttpUtil.get("https://api.oioweb.cn/api/ip/ipaddress", paramMap);
+        return executeGetRequest("https://api.oioweb.cn/api/ip/ipaddress","ip",ip);
     }
 
     // 获取天气
     @GetMapping("weather")
     public String getCityWeather(@RequestParam("city_name") String cityName) {
         log.info("请求到==>/api/weather接口==cityName" + cityName);
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("city_name", cityName);
-        return HttpUtil.get("https://api.oioweb.cn/api/weather/weather", paramMap);
+        return executeGetRequest("https://api.oioweb.cn/api/weather/weather","city_name",cityName);
     }
 
     // 获取手机归属地
     @GetMapping("common/teladress")
     public String getPhoneLocation(@RequestParam("mobile") String mobile) {
         log.info("请求到==>/api/phone接口==mobile:" + mobile);
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("mobile", mobile);
-        return HttpUtil.get("https://api.oioweb.cn/api/common/teladress", paramMap);
+        return executeGetRequest("https://api.oioweb.cn/api/common/teladress","mobile",mobile);
     }
 
     // 获取网站备案信息
     @GetMapping("site/icp")
     public String getSiteIcp(@RequestParam("domain") String domain) {
         log.info("请求到==>/api/site/icp接口==domain:" + domain);
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("domain", domain);
-        return HttpUtil.get("https://api.oioweb.cn/api/site/icp", paramMap);
+        return executeGetRequest("https://api.oioweb.cn/api/site/icp","domain",domain);
     }
 
     // 二维码生成
@@ -64,7 +55,7 @@ public class InterfaceServiceController {
                             @RequestParam(value = "m", required = false) Integer m,
                             @RequestParam(value = "type", required = false) String type,
                             @RequestParam(value = "size", required = false) Integer size) {
-        log.info("请求到==>/api/qrcode/encode接口==text:" + text+",m:"+m+",type:"+type+",size:"+size);
+        log.info("请求到==>/api/qrcode/encode接口==text:" + text + ",m:" + m + ",type:" + type + ",size:" + size);
         // 如果参数为空，设置默认值
         if (m == null) {
             m = 2;
@@ -84,12 +75,56 @@ public class InterfaceServiceController {
     }
 
     // 二维码解析
-    @PostMapping("/qrcode/decode")
+    @PostMapping("qrcode/decode")
     public String getDecode(@RequestPart("file") MultipartFile file) {
-        log.info("请求到==>/api/qrcode/decode接口==file ContentType: " + file.getContentType());
-        log.info("请求到==>/api/qrcode/decode接口==file OriginalFilename: " + file.getOriginalFilename());
+        log.info("请求到==>/api/qrcode/decode接口==file");
+        return handleFileUpload(file, "https://api.oioweb.cn/api/qrcode/decode");
+    }
 
-        // 将 MultipartFile 转换为 File
+    // 每日电影
+    @GetMapping("common/OneFilm")
+    public String OneFilm() {
+        log.info("请求到==>/api/common/OneFilm接口");
+        return HttpUtil.get("https://api.oioweb.cn/api/common/OneFilm");
+    }
+
+    // 看图识物
+    @PostMapping("ocr/recognition")
+    public String recognition(@RequestPart("file") MultipartFile file) {
+        log.info("请求到==>/api/ocr/recognition接口==file");
+        return handleFileUpload(file, "https://api.oioweb.cn/api/ocr/recognition");
+    }
+
+    // 看图识物
+    @PostMapping("search/anilistInfo")
+    public String searchAnimeInfo(@RequestPart("file") MultipartFile file) {
+        log.info("请求到==>/api/search/anilistInfo接口==file");
+        return handleFileUpload(file, "https://api.oioweb.cn/api/search/anilistInfo");
+    }
+
+    // 日读世界60s
+    @GetMapping("common/today")
+    public String getTodayInfo() {
+        log.info("请求到==>/api/common/today接口");
+        return HttpUtil.get("https://api.oioweb.cn/api/common/today");
+    }
+
+    // 热搜榜
+    @GetMapping("common/fetchHotSearchBoard")
+    public String fetchHotSearchBoard(@RequestParam("type") String type) {
+        log.info("请求到==>/api/common/fetchHotSearchBoard接口");
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("type", type);
+        return HttpUtil.get("https://api.oioweb.cn/api/common/fetchHotSearchBoard",paramMap);
+    }
+
+    private <T> String executeGetRequest(String url, String paramName, T paramValue) {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put(paramName, paramValue);
+        return HttpUtil.get(url, paramMap);
+    }
+
+    private String handleFileUpload(MultipartFile file, String url) {
         File tempFile;
         try {
             tempFile = File.createTempFile("upload", file.getOriginalFilename());
@@ -99,14 +134,11 @@ public class InterfaceServiceController {
             return "{\"code\":500,\"result\":null,\"msg\":\"转换文件失败!\"}";
         }
 
-        // 构建参数
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("file", tempFile);
 
-        // 调用外部接口
-        String result = HttpUtil.post("https://api.oioweb.cn/api/qrcode/decode", paramMap);
+        String result = HttpUtil.post(url, paramMap);
 
-        // 删除临时文件
         if (tempFile.exists()) {
             boolean isDeleted = tempFile.delete();
             if (isDeleted) {
@@ -115,7 +147,6 @@ public class InterfaceServiceController {
                 log.error("Failed to delete file");
             }
         }
-
         return result;
     }
 }
