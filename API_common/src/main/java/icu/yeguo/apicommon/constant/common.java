400efa6a0527b15a372c6f2e5d52838c05b1a0df
yeguoz <aidjajd@163.com>
2024-12-20 12:44:48 +0800

    feat: 提取公共常量

---
+++ API_common/src/main/java/icu/yeguo/apicommon/constant/common.java
@@ -0,0 +1,15 @@
+package icu.yeguo.apicommon.constant;
+
+public interface common {
+    String POST = "POST";
+    String GET = "GET";
+    String FILE = "file";
+    String SIGNATURE = "signature";
+    String X_ONLINE_INVOKING = "X-Online-invoking";
+    String X_ACCESS_KEY = "X-Access-Key";
+    String X_FILE_NAME = "X-File-Name";
+    String X_FILE_SIZE = "X-File-Size";
+    String X_EXPIRY_TIMESTAMP = "X-Expiry-Timestamp";
+    String X_NONCE = "X-Nonce";
+    String X_SIGNATURE = "X-Signature";
+}
