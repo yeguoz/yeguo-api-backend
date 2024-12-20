e85037489c4b8d29345cb34c82984ad71e4caa8b
yeguoz <aidjajd@163.com>
2024-12-20 12:43:43 +0800

    refactor: 删除不需要常量

--- API_backend/src/main/java/icu/yeguo/yeguoapi/constant/SecretConstant.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/constant/SecretConstant.java
@@ -1,6 +1,5 @@
 package icu.yeguo.yeguoapi.constant;
 
 public interface SecretConstant {
-    String PASSWORD_SECRET_KEY = "野果_API平台";
-    String SIGNATURE_KEY =  "野果API";
+    String  PASSWORD_SECRET_KEY = "野果_API平台";
 }
