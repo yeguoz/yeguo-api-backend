771f16755d67b4cc071ef5ea0b26cd86715913ce
yeguoz <aidjajd@163.com>
2024-12-22 00:30:08 +0800

    Revert "feat: 空参、全参构造方法"
    This reverts commit 3a2c161b2effd464bcefa62c163c89c12fecc2b2.

--- API_interface/src/main/java/icu/yeguo/yeguoapiinterface/common/Response.java
+++ API_interface/src/main/java/icu/yeguo/yeguoapiinterface/common/Response.java
@@ -1,16 +1,11 @@
 package icu.yeguo.yeguoapiinterface.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Response<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = -3481618647530458478L;
