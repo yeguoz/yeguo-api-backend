bc3a2e90b93fae17e5e11edf97ccd72ac92cf258
yeguoz <aidjajd@163.com>
2024-12-22 00:33:49 +0800

    feat: 空参、全参构造

--- API_interface/src/main/java/icu/yeguo/yeguoapiinterface/common/Response.java
+++ API_interface/src/main/java/icu/yeguo/yeguoapiinterface/common/Response.java
@@ -1,11 +1,15 @@
 package icu.yeguo.yeguoapiinterface.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Response<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = -3481618647530458478L;
