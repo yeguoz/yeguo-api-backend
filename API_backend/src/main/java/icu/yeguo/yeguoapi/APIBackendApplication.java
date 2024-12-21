f0874e94f27382f7e778f19e9b015b12da6f5003
yeguoz <aidjajd@163.com>
2024-12-22 00:01:31 +0800

    refactor: 启用事务

--- API_backend/src/main/java/icu/yeguo/yeguoapi/APIBackendApplication.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/APIBackendApplication.java
@@ -4,10 +4,12 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("icu.yeguo.yeguoapi.mapper")
 @EnableDubbo
+@EnableTransactionManagement
 public class APIBackendApplication {
 
     public static void main(String[] args) {
