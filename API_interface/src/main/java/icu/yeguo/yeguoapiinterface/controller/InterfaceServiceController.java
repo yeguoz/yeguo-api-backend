075bb3e52d49907b83aab21f53f8cfe0d34aa81c
yeguoz <aidjajd@163.com>
2024-12-22 16:52:56 +0800

    refactor: 更新接口

--- API_interface/src/main/java/icu/yeguo/yeguoapiinterface/controller/InterfaceServiceController.java
+++ API_interface/src/main/java/icu/yeguo/yeguoapiinterface/controller/InterfaceServiceController.java
@@ -1,6 +1,8 @@
 package icu.yeguo.yeguoapiinterface.controller;
 
 import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import icu.yeguo.yeguoapiinterface.common.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,108 +16,105 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class InterfaceServiceController {
-    // 不能使用了
+    // 1. qq 信息
     @GetMapping("qq/info")
     public String getQQInfo(@RequestParam("qq") Long qq) {
-        log.info("请求到==>/api/qq/info接口==qq:" + qq);
-        return executeGetRequest("https://api.oioweb.cn/api/qq/info","qq",qq);
+        log.info("请求到==>/api/app/qq/info接口==qq:{}", qq);
+        String result = executeGetRequest("https://api.uomg.com/api/qq.info", "qq", qq);
+        log.info("qq/info:{}", result);
+        return result;
     }
 
-    // 1.获取ip地址
-    @GetMapping("ip/ipaddress")
-    public String getIpAddress(@RequestParam("ip") String ip) {
-        log.info("请求到==>/api/ip/ipaddress接口==ip" + ip);
-        return executeGetRequest("https://api.oioweb.cn/api/ip/ipaddress","ip",ip);
+    // 2.获取ip地址
+    @GetMapping("ipaddress")
+    public String getIpAddress(@RequestParam(value = "ip", required = false) String ip) {
+        log.info("请求到==>/api/app/ipaddress接口==ip:{}", ip);
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("json", "true");
+        paramMap.put("ip", ip);
+        String result = HttpUtil.get("http://whois.pconline.com.cn/ipJson.jsp", paramMap);
+        log.info("ip地址:{}", result);
+        if (result.contains("503"))
+            return JSONUtil.toJsonStr(new Response<>(503, "", "接口不稳定请稍后再试"));
+        return result.strip();
     }
 
-    // 2.获取天气
+    // 3.获取天气
     @GetMapping("weather")
-    public String getCityWeather(@RequestParam("city_name") String cityName) {
-        log.info("请求到==>/api/weather接口==cityName" + cityName);
-        return executeGetRequest("https://api.oioweb.cn/api/weather/weather","city_name",cityName);
+    public String getCityWeather(@RequestParam("city") String city) {
+        log.info("请求到==>/api/app/weather接口==city:{}", city);
+        String result = executeGetRequest("http://shanhe.kim/api/za/tianqi.php", "city", city);
+        log.info("城市天气：{}", result);
+        return result;
     }
 
-    // 3.获取手机归属地
-    @GetMapping("common/teladress")
-    public String getPhoneLocation(@RequestParam("mobile") String mobile) {
-        log.info("请求到==>/api/phone接口==mobile:" + mobile);
-        return executeGetRequest("https://api.oioweb.cn/api/common/teladress","mobile",mobile);
+    // 4.获取手机归属地
+    @GetMapping("phone/location")
+    public String getPhoneLocation(@RequestParam("tel") String tel) {
+        log.info("请求到==>/api/app/phone/location接口==mobile:{}", tel);
+        String result = executeGetRequest("http://shanhe.kim/api/za/phone.php", "tel", tel);
+        log.info("手机归属地：{}", result);
+        return result;
     }
 
-    // 4.获取网站备案信息
-    @GetMapping("site/icp")
-    public String getSiteIcp(@RequestParam("domain") String domain) {
-        log.info("请求到==>/api/site/icp接口==domain:" + domain);
-        return executeGetRequest("https://api.oioweb.cn/api/site/icp","domain",domain);
+
+    // 5.二维码解析
+    @GetMapping("qrcode/decode")
+    public String getQrcodeDecode(@RequestParam("url") String url) {
+        log.info("请求到==>/api/app/qrcode/decode接口==url:{}", url);
+        String result = executeGetRequest("https://api.uomg.com/api/qr.encode", "url", url);
+        log.info("二维码解析：{}", result);
+        return result;
     }
 
-    // 5.二维码生成
-    @GetMapping("qrcode/encode")
-    public String getQrcodeEncode(@RequestParam("text") String text,
-                            @RequestParam(value = "m", required = false) Integer m,
-                            @RequestParam(value = "type", required = false) String type,
-                            @RequestParam(value = "size", required = false) Integer size) {
-        log.info("请求到==>/api/qrcode/encode接口==text:" + text + ",m:" + m + ",type:" + type + ",size:" + size);
-        // 如果参数为空，设置默认值
-        if (m == null) {
-            m = 2;
-        }
-        if (type == null) {
-            type = "svg";
-        }
-        if (size == null) {
-            size = 15;
-        }
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("text", text);
-        paramMap.put("m", m);
-        paramMap.put("type", type);
-        paramMap.put("size", size);
-        return HttpUtil.get("https://api.oioweb.cn/api/qrcode/encode", paramMap);
+    // 6.微博热搜
+    @GetMapping("weibo/hot")
+    public String getWBHotSearch() {
+        log.info("请求到==>/api/app/weibo/hot接口");
+        String result = HttpUtil.get("http://shanhe.kim/api/za/weibo.php");
+        log.info("微博热搜：{}", result);
+        return result;
     }
 
-    // 6.二维码解析
-    @PostMapping("qrcode/decode")
-    public String getQrcodeDecode(@RequestPart("file") MultipartFile file) {
-        log.info("请求到==>/api/qrcode/decode接口==file");
-        return handleFileUpload(file, "https://api.oioweb.cn/api/qrcode/decode");
+    // 7.哔哩哔哩热搜
+    @GetMapping("bilibili/hot")
+    public String getBilibiliHotSearch() {
+        log.info("请求到==>/api/app/bilibili/hot接口");
+        String result = HttpUtil.get("https://api.vvhan.com/api/hotlist/bili");
+        log.info("哔哩哔哩热搜：{}", result);
+        return result;
     }
 
-    // 7.每日电影
-    @GetMapping("common/OneFilm")
-    public String getOneFilm() {
-        log.info("请求到==>/api/common/OneFilm接口");
-        return HttpUtil.get("https://api.oioweb.cn/api/common/OneFilm");
-    }
-
-    // 8.看图识物
-    @PostMapping("ocr/recognition")
-    public String recognition(@RequestPart("file") MultipartFile file) {
-        log.info("请求到==>/api/ocr/recognition接口==file");
-        return handleFileUpload(file, "https://api.oioweb.cn/api/ocr/recognition");
-    }
-
-    // 9.以图识番
-    @PostMapping("search/anilistInfo")
-    public String searchAnimeInfo(@RequestPart("file") MultipartFile file) {
-        log.info("请求到==>/api/search/anilistInfo接口==file");
-        return handleFileUpload(file, "https://api.oioweb.cn/api/search/anilistInfo");
-    }
-
-    // 10.日读世界60s
-    @GetMapping("common/today")
-    public String getTodayInfo() {
-        log.info("请求到==>/api/common/today接口");
-        return HttpUtil.get("https://api.oioweb.cn/api/common/today");
-    }
-
-    // 11.热搜榜
-    @GetMapping("common/fetchHotSearchBoard")
-    public String getHotSearch(@RequestParam("type") String type) {
-        log.info("请求到==>/api/common/fetchHotSearchBoard接口");
+    // 8.每日英语
+    @GetMapping("daily/english")
+    public String getDailyEnglish(@RequestParam(value = "type", required = false) String type) {
+        log.info("请求到==>/api/app/daily/english接口");
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("type", type);
-        return HttpUtil.get("https://api.oioweb.cn/api/common/fetchHotSearchBoard",paramMap);
+        String result = HttpUtil.get("https://api.vvhan.com/api/dailyEnglish", paramMap);
+        log.info("每日英语：{}", result);
+        return result;
+    }
+
+    // 9. IT资讯热榜
+    @GetMapping("it/hot")
+    public String getITHotSearch() {
+        log.info("请求到==>/api/app/it/hot接口");
+        String result = HttpUtil.get("https://api.vvhan.com/api/hotlist/itNews");
+        log.info("IT热搜：{}", result);
+        return result;
+    }
+
+    // 10. 星座运势
+    @GetMapping("horoscope")
+    public String getHoroscope(@RequestParam("type") String type, @RequestParam("time") String time) {
+        log.info("请求到==>/api/app/horoscope接口");
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("type", type);
+        paramMap.put("time", time);
+        String result = HttpUtil.get("https://api.vvhan.com/api/horoscope", paramMap);
+        log.info("星座运势：{}", result);
+        return result;
     }
 
     private <T> String executeGetRequest(String url, String paramName, T paramValue) {
@@ -124,6 +123,7 @@ public class InterfaceServiceController {
         return HttpUtil.get(url, paramMap);
     }
 
+    // 处理上传的文件发起请求
     private String handleFileUpload(MultipartFile file, String url) {
         File tempFile;
         try {
@@ -131,7 +131,7 @@ public class InterfaceServiceController {
             file.transferTo(tempFile);
         } catch (IOException e) {
             log.error("转换文件失败", e);
-            return "{\"code\":500,\"result\":null,\"msg\":\"转换文件失败!\"}";
+            return new Response<>(500, null, "转换文件失败!").toString();
         }
 
         HashMap<String, Object> paramMap = new HashMap<>();
