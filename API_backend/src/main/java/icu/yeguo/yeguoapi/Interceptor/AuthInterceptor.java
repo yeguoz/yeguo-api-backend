b205aaf25a252a56046a01e837375bb370dd3f08
yeguoz <aidjajd@163.com>
2025-01-09 11:58:32 +0800

    feat:AuthInterceptor

--- API_backend/src/main/java/icu/yeguo/yeguoapi/Interceptor/AuthInterceptor.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/Interceptor/AuthInterceptor.java
@@ -9,16 +9,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
-public class LoginInterceptor implements HandlerInterceptor {
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(@NotNull HttpServletRequest request,
                              @NotNull HttpServletResponse response,
                              @NotNull Object handler) {
         // 在处理器方法执行前执行的逻辑
-        log.info("PreHandle: 请求路径 -> {}", request.getRequestURI());
+        log.info("Login Interceptor PreHandle: 请求路径 -> {}", request.getRequestURI());
 
         HttpSession session = request.getSession();
         UserVO currentUser = (UserVO)session.getAttribute(UserConstant.USER_LOGIN_STATE);
--- API_backend/src/main/java/icu/yeguo/yeguoapi/config/WebConfig.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/config/WebConfig.java
@@ -1,30 +1,36 @@
 package icu.yeguo.yeguoapi.config;
 
-import icu.yeguo.yeguoapi.Interceptor.LoginInterceptor;
+import icu.yeguo.yeguoapi.Interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private AuthInterceptor authInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册自定义拦截器并指定拦截路径
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**") // 拦截所有路径
                 .excludePathPatterns(
-                        "/api/user/login",
-                        "/api/user/register",
-                        "/api/user/verifyCode",
-                        "/api/user/emailRegister",
-                        "/api/user/emailLogin",
-                        "/api/user/forgetPwd/verifyCode",
-                        "/api/user/forgetPwd",
-                        "/doc.html",          // 去掉 /api，Swagger 文档路径通常是直接 /doc.html
-                        "/v3/**",              // Swagger 的接口文档通常在 /v3/** 下
-                        "/api/error",
+                        "/user/login",
+                        "/user/register",
+                        "/user/verifyCode",
+                        "/user/emailRegister",
+                        "/user/emailLogin",
+                        "/user/forgetPwd/verifyCode",
+                        "/user/forgetPwd",
+                        "/v3/**",
+                        "/doc.html",
+                        "/webjars/**",
+                        "/swagger-ui.html",
                         "/error"
                 );
+
     }
 }
