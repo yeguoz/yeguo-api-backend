b940d9469406d1d3b844347748a3d933359020b4
yeguoz <aidjajd@163.com>
2025-02-28 16:38:01 +0800

    refactor: 创建订单

--- API_backend/src/main/java/icu/yeguo/yeguoapi/service/impl/OrderInfoServiceImpl.java
+++ API_backend/src/main/java/icu/yeguo/yeguoapi/service/impl/OrderInfoServiceImpl.java
@@ -1,34 +1,58 @@
 package icu.yeguo.yeguoapi.service.impl;
 
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import icu.yeguo.yeguoapi.common.ResponseCode;
 import icu.yeguo.yeguoapi.exception.BusinessException;
 import icu.yeguo.yeguoapi.model.dto.orderInfo.CreateOrderInfoRequest;
+import icu.yeguo.yeguoapi.model.dto.orderInfo.LeavePay;
 import icu.yeguo.yeguoapi.model.dto.orderInfo.OrderInfoQueryRequest;
 import icu.yeguo.yeguoapi.model.entity.OrderInfo;
 import icu.yeguo.yeguoapi.model.vo.OrderInfoVO;
 import icu.yeguo.yeguoapi.service.OrderInfoService;
 import icu.yeguo.yeguoapi.mapper.OrderInfoMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
+
 
 /**
  * @author Lenovo
  * @description 针对表【order_info】的数据库操作Service实现
  * @createDate 2024-07-15 17:02:51
  */
+@Slf4j
 @Service
 public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo>
         implements OrderInfoService {
     @Autowired
     private OrderInfoMapper orderInfoMapper;
+    @Value("${pay.address}")
+    private String payAddress;
+    @Value("${pay.notify.url}")
+    private String notifyUrl;
+    @Value("${pay.return.url}")
+    private String returnUrl;
+    @Value("${pay.site.name}")
+    private String siteName;
+    @Value("${pay.sign.type}")
+    private String signType;
+    @Value("${pay.pid}")
+    private String pid;
+    @Value("${pay.key}")
+    private String key;
 
     @Override
     public List<OrderInfoVO> getUserAllOrderInfos(Long userId) {
@@ -47,23 +71,61 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
         return orderInfoVOList;
     }
 
-    @Transactional
     @Override
     public OrderInfoVO createOrderInfo(CreateOrderInfoRequest createOrderInfoRequest) {
         OrderInfo orderInfo = new OrderInfo();
+        Long userId = createOrderInfoRequest.getUserId();
+        Integer payType = createOrderInfoRequest.getPayType();
+        BigDecimal money = createOrderInfoRequest.getMoney();
+        String commodityContent = createOrderInfoRequest.getCommodityContent();
+        String outTradeNo = getFormattedDateTime() + ThreadLocalRandom.current().nextInt(100000, 1000000);
+        HashMap<String, Object> paramMap = new HashMap<>();
         try {
-            orderInfo.setUserId(createOrderInfoRequest.getUserId());
-            orderInfo.setPayType(createOrderInfoRequest.getPayType());
-            orderInfo.setMoney(createOrderInfoRequest.getMoney());
-            orderInfo.setCommodityContent(createOrderInfoRequest.getCommodityContent());
+            String signStr = "money=" + money +
+                    "&name=" + commodityContent +
+                    "&notify_url=" + notifyUrl +
+                    "&out_trade_no=" + outTradeNo +
+                    "&pid=" + pid +
+                    "&return_url=" + returnUrl +
+                    "&sitename=" + siteName +
+                    "&type=" + (payType == 1 ? "alipay" : "wxpay") + key;
+            String sign = DigestUtil.md5Hex(signStr);
+            // 下单获取订单id
+            paramMap.put("pid", pid);
+            paramMap.put("type", payType == 1 ? "alipay" : "wxpay");
+            paramMap.put("out_trade_no", outTradeNo);
+            paramMap.put("notify_url", notifyUrl);
+            paramMap.put("return_url", returnUrl);
+            paramMap.put("name", commodityContent);
+            paramMap.put("money", money);
+            paramMap.put("sitename", siteName);
+            paramMap.put("sign", sign);
+            paramMap.put("sign_type", signType);
+            String result = HttpRequest.get(payAddress)
+                    .form(paramMap)
+                    .timeout(5 * 60 * 1000)//超时，毫秒
+                    .execute()
+                    .body();
+            LeavePay leavePay = JSONUtil.toBean(result, LeavePay.class);
+            if (leavePay.getCode() == -1) {
+                log.info("下单失败:{}", leavePay.getMsg());
+                throw new BusinessException(ResponseCode.SYSTEM_ERROR, "下单失败");
+            }
+            System.out.println("result:" + result);
+            System.out.println("leavePay:" + leavePay);
+            String orderId = leavePay.getOrderid();
+            System.out.println("leavePay.getOrderid():" + orderId);
+            orderInfo.setOrderId(orderId);
+            orderInfo.setUserId(userId);
+            orderInfo.setPayType(payType);
+            orderInfo.setPayStatus(0);
+            orderInfo.setMoney(money);
+            orderInfo.setCommodityContent(commodityContent);
+            orderInfo.setExpireTime(new Date(System.currentTimeMillis() + 3 * 60 * 1000));
             // 插入数据
-            orderInfoMapper.oderInfoInsert(orderInfo);
-            // 设置orderId
-            orderInfo.setOrderId(getFormattedDateTime() + orderInfo.getId());
-            // 更新订单id
-            orderInfoMapper.updateById(orderInfo);
-            // 查询订单
-            orderInfo = orderInfoMapper.selectById(orderInfo.getId());
+            orderInfoMapper.insert(orderInfo);
+        } catch (BusinessException e) {
+            throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
